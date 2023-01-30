7f0b7db9c816e84e73f335968d60b1bc44531502
Mario Trojan <mario.trojan@uni-tuebingen.de>
2023-01-30 13:20:44 +0100

    UserAuthority: Implement getByAuthorityId() on IxTheo level (differentiate from other instances)

--- module/IxTheo/src/IxTheo/Db/Table/UserAuthority.php
+++ module/IxTheo/src/IxTheo/Db/Table/UserAuthority.php
@@ -3,6 +3,7 @@
 namespace IxTheo\Db\Table;
 
 use Laminas\Db\Sql\Select;
+use IxTheo\Db\Row\UserAuthority as UserAuthorityRow;
 
 class UserAuthority extends \TueFind\Db\Table\UserAuthority {
     public function getAll()
@@ -12,5 +13,19 @@ class UserAuthority extends \TueFind\Db\Table\UserAuthority {
         $select->order('username ASC, authority_id ASC');
         $select->where('user.ixtheo_user_type="' . \IxTheo\Utility::getUserTypeFromUsedEnvironment() . '"');
         return $this->selectWith($select);
+    }
+
+    public function getByAuthorityId($authorityId): ?UserAuthorityRow
+    {
+        $select = $this->getSql()->select();
+        $select->join('user', 'tuefind_user_authorities.user_id = user.id', Select::SQL_STAR, Select::JOIN_LEFT);
+        $select->where('authority_id="' . $authorityId . '"');
+        $select->where('user.ixtheo_user_type="' . \IxTheo\Utility::getUserTypeFromUsedEnvironment() . '"');
+
+        $resultSet = $this->selectWith($select);
+        foreach ($resultSet as $entry) {
+            return $entry;
+        }
+        return null;
     }
 }
