ca5b75cd9df757eb19c07ad0a20ef6360f156162
Steven Lolong <steven.lolong@uni-tuebingen.de>
2023-03-20 10:26:41 +0100

    update number of parameter in RecordDataFormatter

--- module/TueFind/src/TueFind/View/Helper/Root/RecordDataFormatter.php
+++ module/TueFind/src/TueFind/View/Helper/Root/RecordDataFormatter.php
@@ -8,9 +8,9 @@ class RecordDataFormatter extends \VuFind\View\Helper\Root\RecordDataFormatter {
      * Extend parent. Also return options in sub-array, so we are able
      * to pass additional information from Factory to core e.g. core template.
      */
-    protected function render($driver, $field, $data, $options)
+    protected function render($field, $data, $options)
     {
-        $result = parent::render($driver, $field, $data, $options);
+        $result = parent::render($field, $data, $options);
         if (!is_array($result)) {
             return $result;
         } else {
