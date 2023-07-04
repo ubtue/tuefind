091bd6c5af61315d5a576db88a22a354fd6ffe28
Mario Trojan <mario.trojan@uni-tuebingen.de>
2023-07-04 12:54:43 +0200

    doi_notification = doi_notification_datetime

--- module/TueFind/sql/mysql.sql
+++ module/TueFind/sql/mysql.sql
@@ -5,7 +5,7 @@ CREATE TABLE tuefind_publications (
     external_document_id VARCHAR(255) NOT NULL,
     external_document_guid VARCHAR(255) DEFAULT NULL,
     doi VARCHAR(255) DEFAULT NULL,
-    doi_notification DATETIME DEFAULT NULL,
+    doi_notification_datetime DATETIME DEFAULT NULL,
     terms_date DATE NOT NULL,
     publication_datetime TIMESTAMP DEFAULT NOW() NOT NULL,
     PRIMARY KEY (id),
