d1ac76b91d65e08733977c9923e2fd3a7a87b0ca
Steven Lolong <steven.lolong@uni-tuebingen.de>
2023-09-14 08:38:42 +0200

    resolve conflict

--- module/TueFind/src/TueFind/View/Helper/Root/Record.php
+++ module/TueFind/src/TueFind/View/Helper/Root/Record.php
@@ -5,22 +5,11 @@ namespace TueFind\View\Helper\Root;
 class Record extends \VuFind\View\Helper\Root\Record {
 
     /**
-     * This override is a backport to fix issue 2473 in VuFind 8.1.
-     * The fix will be officially included in VuFind 9.0.
-     *
-     * see also: https://github.com/vufind-org/vufind/pull/2841
-     */
-    public function getLink($type, $lookfor)
-    {
-        $link = $this->renderTemplate(
-            'link-' . $type . '.phtml',
-            ['driver' => $this->driver, 'lookfor' => $lookfor]
-        );
-
-        $prepend = (strpos($link, '?') === false) ? '?' : '&amp;';
-
-        $link .= $this->getView()->plugin('searchTabs')
-            ->getCurrentHiddenFilterParams($this->driver->getSourceIdentifier(), false, $prepend);
-        return $link;
-    }
+    * This file originally contained a backport to fix issue 2473 in VuFind 8.1.
+    * The fix has been officially included in VuFind 9.0, so the code has been removed.
+    * We still keep this class since an existing IxTheo class is extending from it,
+    * just in case we need it again in the future.
+    *
+    * see also: https://github.com/vufind-org/vufind/pull/2841
+    */
 }
