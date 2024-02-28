79ea57cb6e222aab7db415ebe57df919b3186240
LysogorAndGmail <LysogorAnd@gmail.com>
2024-02-28 14:33:01 +0100

    fix js error for Ixtheo2

--- themes/ixtheo2/js/ixtheo2.js
+++ themes/ixtheo2/js/ixtheo2.js
@@ -2,16 +2,18 @@ TueFind2.ExpandDropdownsOnHover = true;
 
 var IxTheo2 = {
     ScrollToSearchForm: function() {
-        let content_block_element = $("#content").html();
-        let index_page = $(".index-page").html();
-        let search_form_element = document.getElementById("searchForm");
-        let ix2_search_form = $(".ix2-searchForm").html();
-        if(search_form_element.length > 0 && content_block_element != undefined && index_page == undefined && ix2_search_form != undefined) {
-            const y = search_form_element.getBoundingClientRect().top + window.scrollY;
-            window.scroll({
-            top: y-100,
-            behavior: 'smooth'
-            });
+        if($('#searchForm').length === true) {
+            let content_block_element = $("#content").html();
+            let index_page = $(".index-page").html();
+            let search_form_element = document.getElementById("searchForm");
+            let ix2_search_form = $(".ix2-searchForm").html();
+            if(search_form_element.length > 0 && content_block_element != undefined && index_page == undefined && ix2_search_form != undefined) {
+                const y = search_form_element.getBoundingClientRect().top + window.scrollY;
+                window.scroll({
+                top: y-100,
+                behavior: 'smooth'
+                });
+            }
         }
     },
 }; //end Ixtheo2
