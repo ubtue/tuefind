baf015956e01568c37113d5cd1a7cd3233f20e27
LysogorAndGmail <LysogorAnd@gmail.com>
2024-03-28 09:28:57 +0100

    cheking protocol in URL

--- module/TueFind/src/TueFind/Controller/RedirectController.php
+++ module/TueFind/src/TueFind/Controller/RedirectController.php
@@ -44,12 +44,14 @@ class RedirectController extends \VuFind\Controller\AbstractBase implements \VuF
             // URL needs to be base64, else we will have problems with slashes,
             // even if they are url encoded
             $url = $this->decoder->base64UrlDecode($url);
-            $group = $this->params('group') ?? null;
-            $this->getDbTable('redirect')->insertUrl($url, $group);
-            $view = $this->createViewModel();
-            $view->redirectTarget = $url;
-            $view->redirectDelay = 0;
-            return $view;
+            if (strpos($url, '://') !== false) {
+                $group = $this->params('group') ?? null;
+                $this->getDbTable('redirect')->insertUrl($url, $group);
+                $view = $this->createViewModel();
+                $view->redirectTarget = $url;
+                $view->redirectDelay = 0;
+                return $view;
+            }
         }
 
         $this->getResponse()->setStatusCode(404);
