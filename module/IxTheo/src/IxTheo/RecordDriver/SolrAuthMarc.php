2e9218fe41c9b2d8a81ab33a5cb9df0edc4a8fd0
Mario Trojan <mario.trojan@uni-tuebingen.de>
2024-07-29 16:43:44 +0200

    SolrAuthMarc.php: Change subsystem url schema

--- module/IxTheo/src/IxTheo/RecordDriver/SolrAuthMarc.php
+++ module/IxTheo/src/IxTheo/RecordDriver/SolrAuthMarc.php
@@ -6,13 +6,13 @@ class SolrAuthMarc extends SolrAuthDefault {
     public function getExternalSubsystems(): array
     {
         $subsystemLinks = [
-            ['title' => 'RelBib', 'url' => 'https://relbib.de/Authority/' . urlencode($this->getUniqueID()), 'label' => 'REL'],
-            ['title' => 'Index Biblicus', 'url' => 'https://bible.ixtheo.de/Authority/' . urlencode($this->getUniqueID()), 'label' => 'BIB'],
-            ['title' => 'Index Canonicus', 'url' => 'https://canonlaw.ixtheo.de/Authority/' . urlencode($this->getUniqueID()), 'label' => 'CAN']
+            ['title' => 'RelBib', 'url' => 'https://relbib.de/AuthorityRecord/' . urlencode($this->getUniqueID()), 'label' => 'REL'],
+            ['title' => 'Index Biblicus', 'url' => 'https://bible.ixtheo.de/AuthorityRecord/' . urlencode($this->getUniqueID()), 'label' => 'BIB'],
+            ['title' => 'Index Canonicus', 'url' => 'https://canonlaw.ixtheo.de/AuthorityRecord/' . urlencode($this->getUniqueID()), 'label' => 'CAN']
         ];
 
         $result = [];
-        $result[] = ['title' => 'Index Theologicus', 'url' => 'https://ixtheo.de/Authority/' . urlencode($this->getUniqueID()), 'label' => 'IXT'];
+        $result[] = ['title' => 'Index Theologicus', 'url' => 'https://ixtheo.de/AuthorityRecord/' . urlencode($this->getUniqueID()), 'label' => 'IXT'];
         foreach ($this->getSubsystems() as $subsystem) {
             foreach ($subsystemLinks as $subsystemLink) {
                 if ($subsystemLink['label'] == $subsystem) {
