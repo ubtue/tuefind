e904bc329e3d418c5b1ec6c23c722e1befaa3fd9
Mario Trojan <mario.trojan@uni-tuebingen.de>
2024-10-16 16:55:13 +0200

    VuFind 10.0: Use virtual keyboard from vufind-org

--- themes/tuefind/theme.config.php
+++ themes/tuefind/theme.config.php
@@ -33,21 +33,13 @@ return [
     ],
     'css' => [
         'vendor/jquery-ui.min.css',
-        'vendor/keyboard-basic.css',
-        'vendor/keyboard.css',
-        'vendor/keyboard-dark.css',
         'vendor/jquery.dataTable.css',
-        'vendor/keyboard-previewkeyset.css',
         'botprotect.css',
-        'keyboard-tuefind.css'
     ],
     'js' => [
         'tuefind.js',
         'vendor/jquery-ui.min.js',
-        'vendor/jquery.keyboard.js',
         'vendor/jquery.dataTable.js',
-        'vendor/keyboard-layouts-greywyvern.js',
-        'virtualkeyboard.js'
     ],
     'icons' => [
         'upload' => 'FontAwesome:upload',
