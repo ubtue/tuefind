6d97e9f677dc436fc3d8d88eb43340600dac3588
Mario Trojan <mario.trojan@uni-tuebingen.de>
2024-11-21 09:39:12 +0100

    Optimize translation of [Unassigned] to allow facet filter, see #3055

--- module/TueFind/src/TueFind/Search/Solr/Options.php
+++ module/TueFind/src/TueFind/Search/Solr/Options.php
@@ -2,4 +2,24 @@
 
 namespace TueFind\Search\Solr;
 
-class Options extends \VuFind\Search\Solr\Options {}
+class Options extends \VuFind\Search\Solr\Options {
+    // TueFind-specific: see facets.ini for more detailed description
+    protected $translatedFacetsUnassigned = [];
+
+    public function __construct(\VuFind\Config\PluginManager $configLoader) {
+        parent::__construct($configLoader);
+
+        $facetSettings = $configLoader->get($this->facetsIni);
+
+        if (
+            isset($facetSettings->Advanced_Settings->translated_facets_unassigned)
+            && count($facetSettings->Advanced_Settings->translated_facets_unassigned) > 0
+        ) {
+            $this->translatedFacetsUnassigned = $facetSettings->Advanced_Settings->translated_facets_unassigned->toArray();
+        }
+    }
+
+    public function getTranslatedFacetsUnassigned(): array {
+        return $this->translatedFacetsUnassigned;
+    }
+}
