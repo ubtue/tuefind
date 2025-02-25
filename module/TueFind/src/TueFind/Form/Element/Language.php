7f145a7cb7b1fc0d6a8087a1e018e63c8aede5bd
Steven <steven.lolong@uni-tuebingen.de>
2025-02-25 11:10:06 +0100

    use the default sorting of symfony internationalisation and add three digit codes on the label

--- module/TueFind/src/TueFind/Form/Element/Language.php
+++ module/TueFind/src/TueFind/Form/Element/Language.php
@@ -2,14 +2,19 @@
 
 namespace TueFind\Form\Element;
 
+use Laminas\Validator\Translator\Translator as TranslatorTranslator;
 use Symfony\Component\Intl\Languages;
 
-class Language extends \Laminas\Form\Element\Select {
-    public function __construct() {
+class Language extends \Laminas\Form\Element\Select
+{
+    public function __construct()
+    {
         parent::__construct(...func_get_args());
 
         $languageCodesAndNames = Languages::getAlpha3Names();
-        sort($languageCodesAndNames, SORT_LOCALE_STRING);
+        foreach ($languageCodesAndNames as $code => $name) {
+            $languageCodesAndNames[$code] = $name . ' (' . $code . ')';
+        }
 
         $this->setValueOptions($languageCodesAndNames);
     }
