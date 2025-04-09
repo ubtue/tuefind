0c1b1e25fae9d62a719389d56899fd9934d57bac
root <steven.lolong@uni-tuebingen.de>
2025-04-09 13:56:26 +0200

    remove unused constructor

--- module/KrimDok/src/KrimDok/Search/Params/PluginManager.php
+++ module/KrimDok/src/KrimDok/Search/Params/PluginManager.php
@@ -3,20 +3,10 @@
 namespace KrimDok\Search\Params;
 
 class PluginManager extends \TueFind\Search\Params\PluginManager {
-    // public function __construct($configOrContainerInstance = null,
-    //     array $v3config = []
-    // ) {
-        // $this->aliases['solr'] = \KrimDok\Search\Solr\Params::class;
-        // $this->factories[\KrimDok\Search\Solr\Params::class] = \VuFind\Search\Solr\ParamsFactory::class;
-    //     parent::__construct($configOrContainerInstance, $v3config);
-    // }
-
-
     protected function _addAliasesAndFactories()
     {
         parent::_addAliasesAndFactories();
         $this->aliases['solr'] = \KrimDok\Search\Solr\Params::class;
-        // $this->factories[\KrimDok\Search\Solr\Params::class] = \VuFind\Search\Solr\ParamsFactory::class;
         $this->factories[\KrimDok\Search\Solr\Params::class] = '\VuFindResultsGrouping\Search\Params\Factory::getSolr';
     }
 }
