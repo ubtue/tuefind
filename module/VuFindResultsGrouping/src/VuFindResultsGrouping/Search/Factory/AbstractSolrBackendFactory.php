c33443c36637cfb1577c840321f694a32a2743c6
Steven Lolong <steven.lolong@uni-tuebingen.de>
2025-04-14 16:12:52 +0200

    write three functions to load fulltext search instance for KrimDok

--- module/VuFindResultsGrouping/src/VuFindResultsGrouping/Search/Factory/AbstractSolrBackendFactory.php
+++ module/VuFindResultsGrouping/src/VuFindResultsGrouping/Search/Factory/AbstractSolrBackendFactory.php
@@ -7,10 +7,14 @@
  * @package  Search
  * @author   <dku@outermedia.de>
  */
+
 namespace VuFindResultsGrouping\Search\Factory;
 
 use VuFindResultsGrouping\Backend\Solr\Backend;
 use VuFindSearch\Backend\Solr\Connector;
+use Psr\Container\ContainerInterface;
+use TueFind\Search\Solr\InjectFulltextMatchIdsListener;
+use Laminas\Config\Config;
 
 abstract class AbstractSolrBackendFactory extends \VuFind\Search\Factory\AbstractSolrBackendFactory
 {
@@ -42,7 +46,7 @@ abstract class AbstractSolrBackendFactory extends \VuFind\Search\Factory\Abstrac
         $maxClauses = $this->getIndexConfig('maxBooleanClauses', $pageSize);
         if ($pageSize > 0 && $maxClauses > 0) {
             $backend->setPageSize(min($pageSize, $maxClauses));
-}
+        }
         $backend->setQueryBuilder($this->createQueryBuilder());
         $backend->setSimilarBuilder($this->createSimilarBuilder());
         if ($this->logger) {
@@ -50,5 +54,39 @@ abstract class AbstractSolrBackendFactory extends \VuFind\Search\Factory\Abstrac
         }
         $backend->setRecordCollectionFactory($this->createRecordCollectionFactory());
         return $backend;
+    }
+
+    // These three functions below for KrimDok Compatibility
+    public function __invoke(ContainerInterface $sm, $name, array $options = null)
+    {
+        $this->serviceLocator = $sm;
+        $this->config = $this->serviceLocator->get(\VuFind\Config\PluginManager::class);
+        if ($this->serviceLocator->has(\VuFind\Log\Logger::class)) {
+            $this->logger = $this->serviceLocator->get(\VuFind\Log\Logger::class);
+        }
+        $connector = $this->createConnector();
+        $backend   = $this->createBackend($connector);
+        $backend->setIdentifier($name);
+        $this->createListeners($backend);
+        return $backend;
+    }
+
+
+    protected function createListeners(\VuFindSearch\Backend\Solr\Backend $backend)
+    {
+        parent::createListeners($backend);
+        $events = $this->serviceLocator->get('SharedEventManager');
+        $search = $this->config->get($this->searchConfig);
+        //        if (isset($search->FulltextMatchIds)) {
+        $this->getInjectFulltextMatchIdsListener($backend, $search)->attach($events);
+        //        }
+    }
+
+
+    protected function getInjectFulltextMatchIdsListener(
+        \VuFindSearch\Backend\BackendInterface $backend,
+        Config $search
+    ) {
+        return new InjectFulltextMatchIdsListener($backend);
     }
 }
