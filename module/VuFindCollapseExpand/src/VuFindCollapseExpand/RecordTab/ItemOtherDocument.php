40ddfaa4165cd7a9456728da42b7fb75a2c9fca0
Steven Lolong <steven.lolong@uni-tuebingen.de>
2025-12-09 14:16:57 +0100

    start working on RecordTab 'OtherDocument'

---
+++ module/VuFindCollapseExpand/src/VuFindCollapseExpand/RecordTab/ItemOtherDocument.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * ItemCollapseExpand tab
+ *
+ * @category TueFind
+ * @package  RecordTabs
+ * @author   Steven Lolong <steven.lolong@uni-tuebingen.de>
+ *
+ */
+
+namespace VuFindCollapseExpand\RecordTab;
+
+use VuFind\I18n\Translator\TranslatorAwareInterface;
+use VuFind\I18n\Translator\TranslatorAwareTrait;
+
+class ItemOtherDocument extends \VuFind\RecordTab\AbstractContent implements TranslatorAwareInterface
+{
+    use TranslatorAwareTrait;
+
+    /**
+     * Main configuration
+     */
+    protected $config;
+
+    /**
+     * Search options plugin manager
+     *
+     * @var \VuFind\Search\Options\PluginManager
+     */
+    protected $searchOptionsManager;
+
+    protected $numOfExpandedDoc = 0;
+
+    protected $results = null;
+
+    /**
+     * Constructor
+     *
+     * @param \Laminas\Config\Config               $config Configuration
+     * @param \VuFind\Search\Options\PluginManager $som    Search options plugin
+     * manager
+     */
+    public function __construct(
+        \Laminas\Config\Config $config,
+        \VuFind\Search\Options\PluginManager $som
+    ) {
+        $this->config = $config;
+        $this->searchOptionsManager = $som;
+        $this->accessPermission = 'access.ItemCollapseExpand';
+
+    }
+
+    public function getDescription()
+    {
+        return 'Other Documents (' . $this->numOfExpandedDoc . ')';
+    }
+
+    public function isActive()
+    {
+        $index = $this->config->get('Index');
+        $collapse_expand = $index->get('collapse_expand') !== null ? $index->get('collapse_expand') : false;
+        if ((bool) $collapse_expand === true && $this->driver->isActiveCnEParams()) {
+            if ($this->results == null) {
+                // $results = $this->driver->tryMethod('getOtherDocument', [$this->driver->getContainerTitleSort()]);
+                // $this->numOfExpandedDoc = $results->countExpandedDoc($this->driver->getContainerTitleSort());
+                $expand_field = $index->get('group.expand');
+                $results = $this->driver->tryMethod('getOtherDocument', [$this->driver->getContainerExpandField($expand_field)]);
+                $this->numOfExpandedDoc = $results->countExpandedDoc($this->driver->getContainerExpandField($expand_field));
+
+            }
+            return $this->numOfExpandedDoc > 0 ? true : false;
+        } else {
+            return false;
+        }
+    }
+}
