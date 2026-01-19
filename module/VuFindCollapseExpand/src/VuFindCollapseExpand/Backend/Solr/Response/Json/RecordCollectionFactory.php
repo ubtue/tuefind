569d19cfc516631cc90ec0445a51f99841d0221c
Steven Lolong <steven.lolong@uni-tuebingen.de>
2026-01-19 10:53:57 +0100

    ignore when the 'servicelocator' variable is not an object

--- module/VuFindCollapseExpand/src/VuFindCollapseExpand/Backend/Solr/Response/Json/RecordCollectionFactory.php
+++ module/VuFindCollapseExpand/src/VuFindCollapseExpand/Backend/Solr/Response/Json/RecordCollectionFactory.php
@@ -48,9 +48,13 @@ class RecordCollectionFactory extends \VuFindSearch\Backend\Solr\Response\Json\R
 
         $this->collectionClass = $collectionClass;
 
-        $config = $serviceLocator->get(\VuFindCollapseExpand\Config\CollapseExpand::class);
-        $serDef = $config->getCurrentSettings();
-        $this->expandFieldName = $serDef['expand.field'];
+        // Fulltext search in TueFind doesn't have service locator here 
+        if(is_object($serviceLocator) ) {
+            $config = $serviceLocator->get(\VuFindCollapseExpand\Config\CollapseExpand::class);
+            $serDef = $config->getCurrentSettings();
+            $this->expandFieldName = $serDef['expand.field'];
+        }
+        
     }
 
     /**
