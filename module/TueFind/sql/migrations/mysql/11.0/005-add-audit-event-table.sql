e389ac71432afccea2993ad7a9275bf5eba77b37
Mario Trojan <mario.trojan@uni-tuebingen.de>
2026-06-16 14:00:13 +0200

    Improve MySQL Upgrade Path for 11.0

--- module/TueFind/sql/migrations/mysql/11.0/005-add-audit-event-table.sql
+++ module/TueFind/sql/migrations/mysql/11.0/005-add-audit-event-table.sql
@@ -1,17 +1,1 @@
-CREATE TABLE `audit_event` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `type` varchar(50) NOT NULL,
-  `subtype` varchar(50) NOT NULL,
-  `user_id` int NULL,
-  `session_id` varchar(128) NULL,
-  `username` varchar(255) NULL,
-  `client_ip` varchar(255) NULL,
-  `server_ip` varchar(255) NULL,
-  `server_name` varchar(255) NULL,
-  `message`  varchar(255) NULL,
-  `data` json DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `audit_event_user_id_idx` (`user_id`),
-  CONSTRAINT `audit_event_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+../../../../../VuFind/sql/migrations/mysql/11.0/005-add-audit-event-table.sql
--- module/TueFind/sql/migrations/mysql/11.0/007-modify-audit-event-columns.sql
+++ module/TueFind/sql/migrations/mysql/11.0/007-modify-audit-event-columns.sql
@@ -1,6 +1,1 @@
-ALTER TABLE `audit_event`
-  ADD COLUMN `payment_id` int DEFAULT NULL;
-
-ALTER TABLE `audit_event` ADD KEY `audit_event_payment_id_idx` (`payment_id`);
-ALTER TABLE `audit_event`
-    ADD CONSTRAINT `audit_event_ibfk_2` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`id`) ON DELETE CASCADE;
+../../../../../VuFind/sql/migrations/mysql/11.0/007-modify-audit-event-columns.sql
