573e24fa0a75129d42804cc573506589a3226961
Mario Trojan <mario.trojan@uni-tuebingen.de>
2026-06-24 16:15:40 +0200

    Remove deprecated PluginFactory

--- module/TueFind/src/TueFind/Search/Factory/Search3BackendFactory.php
+++ module/TueFind/src/TueFind/Search/Factory/Search3BackendFactory.php
@@ -11,12 +11,12 @@ class Search3BackendFactory extends AbstractSolrBackendFactory
         $this->mainConfig = $this->searchConfig = $this->facetConfig = 'Search3';
         $this->searchYaml = 'searchspecs3.yaml';
     }
-    
+
     // Additional SolrDefaultBackendFactory-related changes, no longer active
     protected $createRecordMethod = 'getSearch3Record';
 
     public function getSearch3Record($data, $defaultKeySuffix = 'Default')
     {
-        return $this->getSolrRecord($data, 'Search3', $defaultKeySuffix);
+        //return $this->getSolrRecord($data, 'Search3', $defaultKeySuffix);
     }
 }
