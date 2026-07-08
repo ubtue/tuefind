471f2fc99d2137288410d810c993c2587210ee19
Mario Trojan <mario.trojan@uni-tuebingen.de>
2026-07-08 10:41:38 +0200

    Limit CMS pages view to current subsystem

--- module/TueFind/src/TueFind/Db/Entity/Subsystems.php
+++ module/TueFind/src/TueFind/Db/Entity/Subsystems.php
@@ -2,31 +2,30 @@
 
 namespace TueFind\Db\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-use TueFind\Db\Entity\CmsPages;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'tuefind_subsystems')]
 class Subsystems implements SubsystemsEntityInterface
-{   
+{
+    #[ORM\Column(name: 'id', type: 'integer', nullable: false)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    protected int $id;
+
+    #[ORM\Column(name: 'subsystem', type: 'string', length: 50, nullable: false)]
+    protected string $subSystem;
+
+    #[ORM\OneToMany(mappedBy: 'subSystem', targetEntity: CmsPages::class)]
+    protected Collection $cmsPages;
+
     public function __construct()
     {
         $this->cmsPages = new ArrayCollection();
     }
 
-    #[ORM\Column(name: 'id', type: 'integer', nullable: false)]
-    #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
-    protected int $id;
-    
-    #[ORM\Column(name: 'subsystem', type: 'string', length: 50, nullable: false)]
-    protected string $subSystem;
-    
-    #[ORM\OneToMany(mappedBy: 'subSystem', targetEntity: CmsPages::class)]
-    private Collection $cmsPages;
-    
     public function getId(): ?int
     {
         return $this->id ?? null;
@@ -43,4 +42,8 @@ class Subsystems implements SubsystemsEntityInterface
         return $this;
     }
 
+    public function getCmsPages(): Collection
+    {
+        return $this->cmsPages;
+    }
 }
