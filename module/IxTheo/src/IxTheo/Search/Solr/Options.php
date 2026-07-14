029d4f19bf6f95d941d21384da420a0bba040f12
Steven Lolong <steven.lolong@uni-tuebingen.de>
2026-07-14 16:42:40 +0200

    using backend for translation

--- module/IxTheo/src/IxTheo/Search/Solr/Options.php
+++ module/IxTheo/src/IxTheo/Search/Solr/Options.php
@@ -14,15 +14,30 @@ class Options extends \TueFind\Search\Solr\Options
     protected $forceDefaultSortSearches = [];
 
     /**
-     * Constructor, used to parse IxTheo-specific options from searches.ini
+     * Facets whose modal filter matches translated display text
+     *
+     * @var array
+     */
+    protected $localizedFacetFilters = [];
+
+    /**
+     * Constructor, used to parse IxTheo-specific options
      */
     public function __construct(ConfigManagerInterface $configManager)
     {
         parent::__construct($configManager);
         $searchSettings = $configManager->getConfigObject($this->searchIni);
+        // Get the facet settings from the facets.ini file, so that we can use them in the Solr\Params and Solr\ResultsTrait classes.
+        $facetSettings = $configManager->getConfigObject($this->facetsIni);
 
         if (isset($searchSettings->IxTheo->forceDefaultSortSearches)) {
             $this->forceDefaultSortSearches = $searchSettings->IxTheo->forceDefaultSortSearches->toArray();
+        }
+
+        // Taking the localized facet filters from the facets.ini file, so that we can use them in the Solr\Params and Solr\ResultsTrait classes.
+        if (isset($facetSettings->Advanced_Settings->localized_facet_filters)) {
+            $this->localizedFacetFilters
+                = $facetSettings->Advanced_Settings->localized_facet_filters->toArray();
         }
     }
 
@@ -31,7 +46,18 @@ class Options extends \TueFind\Search\Solr\Options
      *
      * @return array
      */
-    public function getForceDefaultSortSearches() {
+    public function getForceDefaultSortSearches()
+    {
         return $this->forceDefaultSortSearches;
+    }
+
+    /**
+     * Get facets whose modal filter matches translated display text.
+     *
+     * @return array
+     */
+    public function getLocalizedFacetFilters(): array
+    {
+        return $this->localizedFacetFilters;
     }
 }
